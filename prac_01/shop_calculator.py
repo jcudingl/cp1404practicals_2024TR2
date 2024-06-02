5b219bdcd1c466d5d5dd5a92102841a04d051472
jcudingl <ding.li@my.jcu.edu.au>
2024-06-02 19:59:22 +0800

    Changed magic number as constant.

--- prac_01/shop_calculator.py
+++ prac_01/shop_calculator.py
@@ -1,3 +1,6 @@
+BONUS = 0.10
+
+
 total_price = 0
 num_of_item = int(input("Number of items: "))
 for i in range(0, num_of_item, 1):
@@ -7,5 +10,5 @@ for i in range(0, num_of_item, 1):
         price = float(input("Price of item: "))
     total_price += price
 if total_price >= 100:
-    total_price = total_price * (1 - 0.1)
+    total_price = total_price * (1 - BONUS)
 print(f"Total price for {num_of_item} items is ${total_price:.2f} ")
