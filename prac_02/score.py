c516f3b67e8d4b63e9df310b7dca028701a295bd
jcudingl <ding.li@my.jcu.edu.au>
2024-06-02 20:36:22 +0800

    Add docstring.

--- prac_02/score.py
+++ prac_02/score.py
@@ -7,12 +7,14 @@ Broken program to determine score status
 
 
 def main():
+    """Main function"""
     score = float(input("Enter score: "))
     status = determine_status(score)
     print(status)
 
 
 def determine_status(score):
+    """Determine status based on score"""
     if score < 0 or score > 100:
         return "Invalid score"
     elif score >= 90:
