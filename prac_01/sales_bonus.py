be61ff812d61351a619d52dd4fcbf37daa088f82
jcudingl <ding.li@my.jcu.edu.au>
2024-06-02 19:56:38 +0800

    Changed magic number as constant.

--- prac_01/sales_bonus.py
+++ prac_01/sales_bonus.py
@@ -3,14 +3,15 @@ Program to calculate and display a user's bonus based on sales.
 If sales are under $1,000, the user gets a 10% bonus.
 If sales are $1,000 or over, the bonus is 15%.
 """
-
+BONUS_HIGH = 0.15
+BONUS_LOW = 0.10
 
 sales = float(input("Enter sales: $"))
 while sales >= 0:
     if sales >= 1000:
-        bonus_rate = 0.15
+        bonus_rate = BONUS_HIGH
     else:
-        bonus_rate = 0.10
+        bonus_rate = BONUS_LOW
     bonus = sales * bonus_rate
     print(f"Bonus: ${bonus}")
     sales = float(input("Enter sales: $"))
