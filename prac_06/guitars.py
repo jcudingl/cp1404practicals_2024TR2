92e8a19c5ac1e9cac90fa76a16e181cfd349c930
jcudingl <ding.li@my.jcu.edu.au>
2024-07-14 20:45:26 +0800

    Finish.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -5,10 +5,9 @@ if name != "":
     year = int(input("Year: "))
     cost = float(input("Cost: $"))
     guitars.append(Guitar(name, year, cost))
-    print(f"{guitars[-1]} added.")
+    print(f"{guitars[-1]} added.\n")
     name = input("Name: ")
-print("\n... snip ...\nThese are my guitars:")
+print("\n... snip ...\n\nThese are my guitars:")
 for i, guitar in enumerate(guitars, 1):
     vintage_string = "(vintage)" if guitar.is_vintage() else ""
     print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
-    # do something with i (the index) and guitar (the element)
